bdcc7fbae727aeb76cdb6262759a541d3d410add
Shiham Samsudeen <shihams247@gmail.com>
2016-01-14 23:36:18 +0000

    Added validation and create method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,8 +3,20 @@ class ContactsController < ApplicationController
        @contact = Contact.new
    end
    def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+         flash[:success] = "Message Sent"
+         redirect_to new_contact_path
+      else
+         flash[:danger] ="Error occuted. Message not sent."
+         redirect_to new_contact_path
+      end
    end
        
+   private
+      def contact_params
+         params.require(:contact).permit(:name,:email, :comments)
+      end
 end
     
     
