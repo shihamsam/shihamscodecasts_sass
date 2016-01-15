6d96212ca27bebca2754f897836a5993920e3202
Shiham Samsudeen <shihams247@gmail.com>
2016-01-15 00:56:31 +0000

    Updated actionmailer settings

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
       if @contact.save
          name = params[:contact][:name]
          email = params[:contact][:email]
-         body = params[:contact][:body]
+         body = params[:contact][:comments]
          
          ContactMailer.contact_email(name,email,body).deliver
          
