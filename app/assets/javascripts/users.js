2e8976d46d0b4813212b08fd2f00dc1cffd18af5
Shiham Samsudeen <shihams247@gmail.com>
2016-01-17 00:32:47 +0000

    Added stripe support

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    Stripe.setPublishableKey($('meta[]name="stripe-key"').attr('content'));
+    
+    $("#form-submit-btn").click(function(event){
+        
+        event.preventDefault();
+        
+        $('input[type=submit').prop('disabled', true);
+        
+        var error = false;
+        var ccNum = $('#card_number').val(),
+         ccvNum = $('#card_code').val(),
+         expMonth = $('#card_month').val(),
+         expYear = $('#card_year').val();
+         
+         if(!error){
+            Stripe.createToken({
+                number:ccNum,
+                ccv:ccvNum,
+                exp_month:expMonth,
+                exp_year:expYear
+                
+            }, stripeRespondHandler);   
+         }
+         
+         return false;
+        
+    }); // form submission
+    
+    function stripeRespondHandler(){
+        //get a reference to the form:
+        var f = $('#new_user');
+        
+        //get the token from the response:
+        var token = response.id;
+        
+        //add the token to the Form:
+        f.append('<input type="hidden" name="user[stripe_card_token]" value"'+ token + '" />');
+        
+        f.get(0).submit();
+        
+        
+    }
+    }
+    
+});
--- config/initializers/figaro.rb
+++
@@ -1,2 +0,0 @@
-stripe.apikey = ENV["stripe_api_key"]
-STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
