a33ac3d6193e5f65ce9230b4f08697271a3fce9a
Shiham Samsudeen <shihams247@gmail.com>
2016-01-21 12:36:58 +0000

    Stripe updated with Devise code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::ResgitrationsController < Devise:: ResgitrationsController
+    
+    before_filter :select_plan, only: :new
+    
     def create
         super do |resource| 
             if params[:plan]
@@ -12,5 +15,12 @@ class Users::ResgitrationsController < Devise:: ResgitrationsController
         end
       
     end
+    private
+        def select_plan
+           unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+               flash[:notice] = "Please select a membership plan to sign up."
+               redirect_to root_url
+           end 
+        end
        
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ActiveRecord::Base
            :recoverable, :rememberable, :trackable, :validatable
            
   belongs_to :plan
+  attr_accessor :stripe_card_token
   
-  
+
   def save_with_payments
     if valid?
       customer = Stripe::Customer.create(description: email,plan: plan_id, card: stripe_card_token)
